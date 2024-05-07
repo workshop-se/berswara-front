674f3786e0d0eea7dfb1d790e99ffb9268f66d45
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-05-07 16:46:35 +0700

    feat:add login signup button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,12 @@ export default function Header() {
     { name: "Forum", url: "/" },
     { name: "Profile", url: "/" },
   ];
+
+  let authButton = [
+    { name: "Sign up", url: "/" },
+    { name: "Log in", url: "/" },
+  ]
+
   return (
     <nav className="flex h-[70px]">
       <div className="m-auto">Logo</div>
@@ -17,7 +23,10 @@ export default function Header() {
           <Link href={item.url} className="p-[10px]">{item.name}</Link>
         ))}
       </div>
-      <div className="m-auto">Login signup</div>
+      <div className="m-auto w-[206px] h-[36px] rounded-[10px] bg-red-100 mx-[12px] grid grid-cols-2 overflow-hidden">
+        <Link href={authButton[0].url} className="bg-gray-800 flex items-center justify-center text-white">{authButton[0].name}</Link>
+        <Link href={authButton[1].url} className="bg-red-700 flex items-center justify-center text-white">{authButton[1].name}</Link>
+      </div>
     </nav>
   );
 }
