a75f58ec13ae474d666e162d80a6b75a417f8ee2
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-05-07 15:59:27 +0700

    feat: add navigations with no specific links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 export default function Header() {
+  let navItems = [
+    { name: "Home", url: "/" },
+    { name: "Candidates", url: "/" },
+    { name: "News", url: "/" },
+    { name: "How to Vote", url: "/" },
+    { name: "Forum", url: "/" },
+    { name: "Profile", url: "/" },
+  ];
   return (
-    <div>
-      Todo: Header
-    </div>
+    <nav className="flex h-[70px]">
+      <div className="m-auto">Logo</div>
+      <div className="grow flex justify-center m-auto">
+        {navItems.map((item) => (
+          <Link href={item.url} className="p-[10px]">{item.name}</Link>
+        ))}
+      </div>
+      <div className="m-auto">Login signup</div>
+    </nav>
   );
 }
