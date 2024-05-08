7c51b3f26ca2e7f23a0fe0888f9cf807aebd5758
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-05-08 16:18:56 +0700

    feat: init footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import { Jost, Sen } from "next/font/google";
+const jost = Jost({weight:"400", subsets: ["latin"] });
+const sen = Sen({weight:"400", subsets: ["latin"] });
+
+let medias = [
+  { name: "Facebook", src: "/images/facebook.svg" },
+  { name: "Twitter", src: "/images/twitter.svg" },
+  { name: "Youtube", src: "/images/youtube.svg" },
+  { name: "Medium", src: "/images/medium.svg" },
+]
+
 export default function Footer() {
   return (
-    <div>
-      Todo: Footer
-    </div>
+    <footer className="bg-gray-800 text-white px-[150px]">
+      <Image className="pt-[59px] " src="/images/logo-white.svg" height={59} width={260} alt="Logo" />
+      <p className={`${jost.className} text-[24px] max-w-[600px] pb-[90px]`}>Sentral Senayan II Lantai 28, Jl. Asia Afrika No. 8, Gelora, Tanah Abang, RT.1/RW.3, Gelora, Tanah Abang, Kota Jakarta Pusat, DKI Jakarta</p>
+      <div className="flex pb-[28px]">
+        <p className={`${sen.className} text-[16px] grow`}>© 2024 Berswara -  All rights reserved</p>
+        <div className="flex">
+          {medias.map((media) => (
+            <Image className="mx-[8px]" src={media.src} height={28} width={28} alt={media.name} />
+          ))}
+        </div>
+      </div>
+    </footer>
   );
 }
