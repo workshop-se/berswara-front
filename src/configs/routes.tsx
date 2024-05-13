1c5655bf9b9c43ac3dff24b7048d7496d5ea2131
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-05-13 12:17:52 +0700

    ref:move routeMaps to configs

---
+++ src/configs/routes.tsx
@@ -0,0 +1,15 @@
+let navItems = [
+  { name: "Home", url: "/" },
+  { name: "Candidates", url: "/candidates" },
+  { name: "News", url: "/news" },
+  { name: "How to Vote", url: "/howtovote" },
+  { name: "Forum", url: "/forum" },
+  { name: "Profile", url: "/profile" },
+];
+
+let authButton = [
+  { name: "Sign up", url: "/auth/signup" },
+  { name: "Log in", url: "/auth/login" },
+]
+
+export {navItems, authButton}
