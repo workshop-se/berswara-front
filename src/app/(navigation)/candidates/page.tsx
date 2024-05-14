72bdf10bcd875ea5036a507da28b13be6f0621f9
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-05-14 10:31:25 +0700

    feat:init candidates pages tasks

---
+++ src/app/(navigation)/candidates/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Kenali Kandidatmu</h1>
+      <div>Todo: Search bar</div>
+      <div>Todo: Candidate lists</div>
+    </div>
+  );
+}
+
