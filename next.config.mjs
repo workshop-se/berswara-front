8cb8222ae63215ea5c2d7f5a2034537f9327c18b
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-05-14 15:10:19 +0700

    feat:create items and thumbnail

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cloudflare-ipfs.com', 'avatars.githubusercontent.com'],
+    domains: ['cloudflare-ipfs.com', 'avatars.githubusercontent.com', 'loremflickr.com'],
   },
 };
 
--- src/app/(navigation)/forum/page.tsx
+++ src/app/(navigation)/forum/page.tsx
@@ -1,12 +1,17 @@
 import { faker } from '@faker-js/faker';
 import Link from 'next/link';
+import Image from 'next/image';
 
-const newses = Array.from({ length: 4 }, () => ({
+const forums = Array.from({ length: 12 }, () => ({
   title: faker.lorem.sentence(),
-  description: faker.lorem.paragraph(),
+  description: faker.lorem.paragraph(20),
   isOngoing: faker.datatype.boolean(),
-  tags: Array.from({ length: 3 }, () => faker.lorem.word()),
-  link: faker.internet.url()
+  date: faker.date.recent().toDateString(),
+  numComments: faker.datatype.number(100),
+  minRead: faker.datatype.number(10),
+  author: faker.name.firstName() + " " + faker.name.lastName(),
+  authorAvatar: faker.image.avatar(),
+  thumbnail: faker.image.image(),
 }))
 
 export default function Page() {
@@ -14,9 +19,33 @@ export default function Page() {
     <div className='w-[1128px] m-auto pt-[105px]'>
       <h1 className='text-[64px]'>Baca dan berswara dalam diskusi forum!</h1>
       <p className='text-[24px] pb-[60px]'>Come join the team!</p>
-      <div className='flex w-[1128px]'>
-        <div className='grow'>
-          todo: reads
+      <div className='flex w-[1128px] gap-x-[48px]'>
+        <div className='grow flex flex-col gap-y-[32px] rounded-[16px]'>
+          {forums.map((forum) => (
+            <div className='bg-white flex overflow-hidden h-[304px] rounded-[16px] drop-shadow-md' key={forum.date}>
+              <div className='w-[238px] relative'>
+                <Image
+                  className='object-cover'
+                  src={forum.thumbnail}
+                  alt={forum.title}
+                  layout='fill'
+                />
+              </div>
+              <div className='grow p-[24px] flex flex-col'>
+                <div>
+                  todo:head
+                </div>
+
+                <div className='grow'>
+                  todo:content
+                </div>
+
+                <div>
+                  todo:foot
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
         <div>
           <div>
