3178dbd113c7cfaa9c85b0492bbe691fa30e466b
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-02 00:23:07 +0700

    style:reapply real design

--- src/app/(navigation)/profile/page.tsx
+++ src/app/(navigation)/profile/page.tsx
@@ -26,62 +26,63 @@ export default function Page() {
   return (
     <main className="flex justify-center p-[40px]">
       <FormCard>
-        <div className="flex">
+        <div className="flex w-[857px]">
           <div>
             <Image src="/photos/avatar-example.png" alt="hero" width={110} height={110} />
           </div>
-          <form className="pl-[40px]">
-            <div className="grid grid-cols-2 gap-[15px]">
+          <form className="pl-[40px] grow">
+            <div className="flex flex-col gap-[15px]">
               <div className="flex flex-col">
                 <label htmlFor="firstName" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Nama Depan</label>
-                <input type="text" id="firstName" className="w-[160px] h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
-              </div>
-              <div className="flex flex-col">
-                <label htmlFor="lastName" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Nama Belakang</label>
-                <input type="text" id="lastName" className="w-[160px] h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
+                <input type="text" id="firstName" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
               </div>
 
-              <div className="col-span-2 flex flex-col">
+              <div className="flex flex-col">
                 <label htmlFor="email" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Email Address</label>
                 <input type="email" id="email" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
               </div>
 
-              <div className="col-span-2 flex flex-col">
+              <div className="flex flex-col">
                 <label htmlFor="nik" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>NIK</label>
                 <input type="number" id="nik" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
               </div>
 
 
-              <div className="col-span-2 flex flex-col">
+              <div className="flex flex-col">
                 <label htmlFor="address" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Alamat KTP</label>
                 <input type="text" id="address" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
               </div>
 
-              <div className="col-span-2 flex flex-col">
+              <div className="flex flex-col">
                 <label htmlFor="telephone" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>No. Telepon</label>
                 <input type="text" id="telephone" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]" />
               </div>
 
-              <div className="flex flex-col">
-                <label htmlFor="city" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Kota/Kabupaten</label>
-                <select id="city" className="w-[160px] h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]">
-                  {cities.map((city) => (
-                    <option key={city} value={city}>{city}</option>
-                  ))}
-                </select>
+              <div className="flex gap-x-[38px]">
+                <div className="grow flex flex-col">
+                  <label htmlFor="city" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Kota/Kabupaten</label>
+                  <select id="city" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]">
+                    {cities.map((city) => (
+                      <option key={city} value={city}>{city}</option>
+                    ))}
+                  </select>
+                </div>
+
+                <div className="grow flex flex-col">
+                  <label htmlFor="province" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Provinsi</label>
+                  <select id="province" className="h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]">
+                    {provinces.map((province) => (
+                      <option key={province} value={province}>{province}</option>
+                    ))}
+                  </select>
+                </div>
               </div>
 
-              <div className="flex flex-col">
-                <label htmlFor="province" className={`text-[#858585] ${poppinsReg.className} text-[12px]`}>Provinsi</label>
-                <select id="province" className="w-[160px] h-[41px] ring-1 ring-[#C9C9C9] rounded-[3.23px]">
-                  {provinces.map((province) => (
-                    <option key={province} value={province}>{province}</option>
-                  ))}
-                </select>
+              <div className="flex justify-end gap-x-[16px]">
+                <button type="submit" className={`w-[116.11px] text-white items-center justify-center h-[41px] bg-[#D30D19] ${poppinsMed.className}`}>Save</button>
+                <button type="reset" className={`w-[116.11px] text-[#D30D19] items-center justify-center h-[41px] ring-1 ring-[#D30d19] ${poppinsMed.className}`}>Cancel</button>
               </div>
 
-              <button type="submit" className={`flex flex-col rounded-[30px] text-white items-center justify-center h-[41px] bg-[#D30D19] ${poppinsMed.className}`}>Save</button>
-              <button type="reset" className={`flex flex-col rounded-[30px] text-[#D30D19] items-center justify-center h-[41px] ring-1 ring-[#D30d19] ${poppinsMed.className}`}>Cancel</button>
             </div>
           </form>
         </div>
