ceee8fe6aff79b5cee154bdd8b98806228e16e4e
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-01 23:39:35 +0700

    ref:fix linring error

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -4,9 +4,9 @@ import { cookies } from "next/headers";
 const HOST_AD = process.env.HOST_AD || 'http://localhost:3501';
 
 function parseJwt(token: string) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
 
