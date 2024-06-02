ba6d72feb974ca1d9e64eeaf53c3e12f964fb67e
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-02 22:51:28 +0700

    feat(forum): add forum widget

--- src/app/(navigation)/forum/page.tsx
+++ src/app/(navigation)/forum/page.tsx
@@ -1,7 +1,10 @@
+import ForumWidget from "@/ui/ForumWidget";
+
 export default function Page() {
   return (
-    <div>
-      TODO:Content
+    <div className="flex gap-x-[45px] px-[45px] py-[40px]">
+      <div className="grow">Todo: Content</div>
+      <ForumWidget></ForumWidget>
     </div>
   );
 }
---
+++ src/ui/ForumWidget.tsx
@@ -0,0 +1,40 @@
+import { faker } from "@faker-js/faker";
+import Link from "next/link";
+
+export default function ForumWidget() {
+  const mustReadPosts = [
+    { title: faker.lorem.words(3), link: faker.string.uuid() },
+    { title: faker.lorem.words(3), link: faker.string.uuid() },
+    { title: faker.lorem.words(3), link: faker.string.uuid() },
+  ]
+
+  const featuredLinks = [
+    { title: faker.lorem.words(3), link: faker.string.uuid() },
+    { title: faker.lorem.words(3), link: faker.string.uuid() },
+    { title: faker.lorem.words(3), link: faker.string.uuid() },
+  ]
+  return (
+    <div className="w-[270px] py-[30px] px-[20px] bg-white shadow rounded-[5px] flex flex-col gap-y-[40px]">
+      <SubWidget title="Must Read Posts" lists={mustReadPosts} />
+      <SubWidget title="Featured Links" lists={featuredLinks} />
+    </div>
+  );
+}
+
+function SubWidget({ title, lists }: { title: string, lists: { title: string, link: string }[] }) {
+  return (
+    <div className="">
+      <h2 className="text-[15px] pb-[10px]">{title}</h2>
+      <div className="h-[1px] bg-gray-200 w-full mb-[5px]"></div>
+      <ul className="list-disc list-inside text-[13px] flex flex-col gap-y-[10px]">
+        {lists.map((list) => (
+          <li key={list.title} className="text-[#B30D19]">
+            <Link href={`/forum/${list.link}`}>
+              {list.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
