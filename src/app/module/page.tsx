20c7ca4cb30c2c3b380f802ca4188e10e7c10893
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-02 10:47:20 +0700

    style(module): apply hover animation

--- src/app/module/page.tsx
+++ src/app/module/page.tsx
@@ -15,13 +15,19 @@ export default function Page() {
       <p className='text-24px w-[851px]'>Our brand identity system expresses our spirits and inspires our behaviors. The current system consists of 9 core elements that help unify and communicate Campoal brand with consistency and clarity.</p>
       <div className='grid grid-cols-3 mt-[75px] mb-[120px] gap-x-[27px] gap-y-[47px]'>
         {modules.map((module) => (
-          <div key={module.name} className='w-[359px] bg-white rounded-[16px] flex flex-col overflow-hidden drop-shadow-md'>
-            <div className='bg-[#F5EFE0] h-[359px] overflow-hidden flex place-items-center'>
-              <Image className='object-cover w-[128px] h-[128px] m-auto' src={module.icon} alt={module.name} width={0} height={0}/>
+          <div key={module.name} className='relative' >
+            <div
+              className=' relative z-10 w-[359px] h-full bg-white rounded-[16px] flex flex-col overflow-hidden drop-shadow-md transform transition-transform duration-200 hover:-translate-x-[7px] hover:-translate-y-[6px] hover:bg-gray-300'
+            >
+              <div className='bg-[#F5EFE0] h-[359px] overflow-hidden flex place-items-center'>
+                <Image className='object-cover w-[128px] h-[128px] m-auto' src={module.icon} alt={module.name} width={0} height={0} />
+              </div>
+              <div className='p-[24px]'>
+                <h2 className='text-center text-[24px]'>{module.name}</h2>
+                <p className='text-center text-[18px]'>{module.description}</p>
+              </div>
             </div>
-            <div className='p-[24px]'>
-              <h2 className='text-center text-[24px]'>{module.name}</h2>
-              <p className='text-center text-[18px]'>{module.description}</p>
+            <div className="absolute top-0 left-0 z-0 w-full h-full bg-[#CCC0B2] rounded-[16px] flex items-center justify-center text-gray-500">
             </div>
           </div>
         ))}
