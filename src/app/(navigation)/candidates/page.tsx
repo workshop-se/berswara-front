36362d252bcb7b0d157096ee02a153ec88204965
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-03 16:47:57 +0700

    feat(landing): add calon kandidat cards

--- src/app/(navigation)/candidates/page.tsx
+++ src/app/(navigation)/candidates/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
-import Image from 'next/image';
 import { faker } from '@faker-js/faker';
-import Link from 'next/link';
 import { useState, useEffect } from 'react';
+import CandidateCards from '@/ui/CandidateCards';
 
 interface Candidate {
   name: string;
@@ -13,7 +12,6 @@ interface Candidate {
 }
 
 export default function Page() {
-  const [hoveredId, setHoveredId] = useState<string | null>(null);
   const [candidates, setCandidates] = useState<Candidate[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>('');
 
@@ -46,43 +44,7 @@ export default function Page() {
         <input className="grow focus:outline-none" type="text" placeholder="Cari kandidat" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
       </div>
 
-      <div className='grid grid-cols-4 mt-[75px] mb-[120px] gap-x-[24px] gap-y-[116px]'>
-        {filteredCandidates.map((candidate) => (
-          <Link key={candidate.id} className='h-[354px] relative' href={`/candidates/${candidate.id}`} >
-            <div
-              className="relative rounded-[10px] overflow-hidden shadow-md"
-              onMouseEnter={() => setHoveredId(candidate.id)}
-              onMouseLeave={() => setHoveredId(null)}
-            >
-              <div className="w-[263px] h-[346px] relative">
-                <Image
-                  className="object-cover"
-                  src={candidate.avatar}
-                  alt={candidate.name}
-                  layout='fill'
-                />
-              </div>
-
-              {hoveredId === candidate.id ? (
-                <div className='absolute top-0 left-0 w-full h-full bg-white bg-gradient-to-b from-[#FFFCF7] to-[#DBCCC1] flex backdrop-filter backdrop-blur-sm opacity-95 rounded-[10px] transition-all duration-300'>
-                  <div className='flex flex-col justify-center items-center w-full h-full px-auto'>
-                    <h2 className='text-[18px] text-center'>{candidate.name}</h2>
-                    <h3 className='text-[14px] text-center text-[#5F5F75]'>{candidate.party}</h3>
-                    <h4 className='bg-[#B30D19] text-white rounded-[10px] text-center p-2 mt-[10px]'>Read More</h4>
-                  </div>
-                </div>
-              ) : (
-                <div className='absolute bottom-0 left-0 w-full bg-white backdrop-filter backdrop-blur-sm opacity-95 rounded-[10px] transition-all duration-300'>
-                  <div className='flex flex-col justify-center items-center p-4'>
-                    <h2 className='text-[18px] text-center'>{candidate.name}</h2>
-                    <h3 className='text-[14px] text-center text-[#5F5F75]'>{candidate.party}</h3>
-                  </div>
-                </div>
-              )}
-            </div>
-          </Link>
-        ))}
-      </div>
+      <CandidateCards candidates={filteredCandidates} />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Link from "next/link";
 import Image from 'next/image';
+import CandidateCards from "@/ui/CandidateCards";
+import { faker } from "@faker-js/faker";
+import { useEffect, useState } from "react";
 // import React, { useRef, useState } from 'react';
 // // Import Swiper React components
 // import { Swiper, SwiperSlide } from 'swiper/react';
@@ -12,12 +17,31 @@ import Image from 'next/image';
 // // import required modules
 // import { Pagination } from 'swiper/modules';
 
+interface Candidate {
+  name: string;
+  id: string;
+  avatar: string;
+  party: string;
+}
+
 const heroButton = [
-  {name : "Get Started", url : "/"},
-  {name : "Learn More", url : "/"}
+  { name: "Get Started", url: "/" },
+  { name: "Learn More", url: "/" }
 ]
 
 export default function Home() {
+  const [candidates, setCandidates] = useState<Candidate[]>([]);
+
+  useEffect(() => {
+    const newCandidates: Candidate[] = Array.from({ length: 12 }, () => ({
+      name: faker.person.fullName(),
+      id: faker.string.uuid(),
+      avatar: faker.image.avatar(),
+      party: faker.company.name()
+    }));
+    setCandidates(newCandidates);
+  }, []);
+
   return (
     <div className="">
       <div>
@@ -27,7 +51,7 @@ export default function Home() {
           <p className="text-[24px] text-center font-bold">Satu suara sangat berarti.</p>
         </div>
         <div className="w-[818px] h-[632px] relative mx-auto">
-          <Image className="mt-6" src="/images/hero.svg" alt="hero" width={0} height={0} layout="fill"/> 
+          <Image className="mt-6" src="/images/hero.svg" alt="hero" width={0} height={0} layout="fill" />
           <div className="flex justify-center m-[52px] z-10 relative">
             <Link href={heroButton[0].url} className="mx-[16.5px] text-white bg-[#B30D19] py-[13px] px-[49px] rounded-[27px]">
               {heroButton[0].name}
@@ -40,12 +64,12 @@ export default function Home() {
       </div>
 
       <div className="mt-[155px]">
-          <h1 className="text-[72px] text-center font-bold">Baca berita terbaru hari ini!</h1>
-          <p className="text-[24px] text-center font-bold">Berita seputar pemilu anti hoaks hoaks gacor.</p>
-          <div className="text-center">
-            todo : image slide
-          </div>
-      </div>      
+        <h1 className="text-[72px] text-center font-bold">Baca berita terbaru hari ini!</h1>
+        <p className="text-[24px] text-center font-bold">Berita seputar pemilu anti hoaks hoaks gacor.</p>
+        <div className="text-center">
+          todo : image slide
+        </div>
+      </div>
       <div className="bg-[#FFFCF7]">
         <div className="grid grid-col-2">
           <div className="col-span-2 mb-[30px]">
@@ -54,78 +78,78 @@ export default function Home() {
           </div>
           <div className="flex flex-col ml-[200px]">
             <div className="flex flex-row mb-[50px]">
-                <div className="mr-[29px]">
-                    <Image className="object-cover w-[72px] h-[72px]" src="/images/icon-modul.svg" alt="icon modul" width={0} height={0} />
-                </div>
-                <div className="w-[389px]">
-                    <h1 className="text-[24px] font-bold text-left">Modul Belajar Pemilu</h1> 
-                    <p className="text-[18px] text-[#5F5F75] text-left">Belajar langkah-langkah dan prosedur pemilihan umum.</p>
-                </div>
+              <div className="mr-[29px]">
+                <Image className="object-cover w-[72px] h-[72px]" src="/images/icon-modul.svg" alt="icon modul" width={0} height={0} />
+              </div>
+              <div className="w-[389px]">
+                <h1 className="text-[24px] font-bold text-left">Modul Belajar Pemilu</h1>
+                <p className="text-[18px] text-[#5F5F75] text-left">Belajar langkah-langkah dan prosedur pemilihan umum.</p>
+              </div>
             </div>
             <div className="flex flex-row mb-[50px]">
-                <div className="mr-[29px]">
-                    <Image className="object-cover w-[72px] h-[72px]" src="/images/icon-quiz.svg" alt="icon quiz" width={0} height={0} />
-                </div>
-                <div className="w-[389px]">
-                    <h1 className="text-[24px] font-bold text-left">Quiz Interaktif</h1> 
-                    <p className="text-[18px] text-[#5F5F75] text-left">Uji pengetahuan kebangsaan dan pengetahuan politikmu!</p>
-                </div>
+              <div className="mr-[29px]">
+                <Image className="object-cover w-[72px] h-[72px]" src="/images/icon-quiz.svg" alt="icon quiz" width={0} height={0} />
+              </div>
+              <div className="w-[389px]">
+                <h1 className="text-[24px] font-bold text-left">Quiz Interaktif</h1>
+                <p className="text-[18px] text-[#5F5F75] text-left">Uji pengetahuan kebangsaan dan pengetahuan politikmu!</p>
+              </div>
             </div>
             <div className="flex flex-row mb-[50px]">
-                <div className="mr-[29px]">
-                    <Image className="object-cover w-[72px] h-[72px]" src="/images/icon-kandidat.svg" alt="icon kandidat" width={0} height={0} />
-                </div>
-                <div className="w-[389px]">
-                    <h1 className="text-[24px] font-bold text-left">Kenali Calon Kandidatmu!</h1> 
-                    <p className="text-[18px] text-[#5F5F75] text-left">Kenali calon kandidat yang mewakili suaramu!</p>
-                </div>
+              <div className="mr-[29px]">
+                <Image className="object-cover w-[72px] h-[72px]" src="/images/icon-kandidat.svg" alt="icon kandidat" width={0} height={0} />
+              </div>
+              <div className="w-[389px]">
+                <h1 className="text-[24px] font-bold text-left">Kenali Calon Kandidatmu!</h1>
+                <p className="text-[18px] text-[#5F5F75] text-left">Kenali calon kandidat yang mewakili suaramu!</p>
+              </div>
             </div>
           </div>
           <div>
-            <Image src="/images/hero-2.svg" alt="hero-2" width={0} height={0} className="object-scale-down w-[637px]"/>
+            <Image src="/images/hero-2.svg" alt="hero-2" width={0} height={0} className="object-scale-down w-[637px]" />
           </div>
         </div>
       </div>
 
       <div className="bg-[#FFFCF7] pt-[150px]">
         <div className="flex flex-col justify-center">
-            <h1 className="text-[48px] text-center font-bold">Modul Prosedur Proses Pemilihan</h1>
-            <div className="flex justify-center pb-[53px]">
-              <p className="text-[18px] text-[#5F5F75] w-[577px] text-center">Berikut adalah langkah-langkah yang harus anda lakukan agar dapat mengikuti pemilihan dan menggunakan hak suara anda.</p>
-            </div>
+          <h1 className="text-[48px] text-center font-bold">Modul Prosedur Proses Pemilihan</h1>
+          <div className="flex justify-center pb-[53px]">
+            <p className="text-[18px] text-[#5F5F75] w-[577px] text-center">Berikut adalah langkah-langkah yang harus anda lakukan agar dapat mengikuti pemilihan dan menggunakan hak suara anda.</p>
+          </div>
         </div>
-       <div className="flex flex-auto justify-center">
-            <div className="flex flex-row gap-6">
-              <div className="bg-[#F5EFE0] w-[359px] h-[448px] rounded-[16px]">
-                <div className="flex flex-col items-center mt-[37px] ">
-                  <Image className="object-cover w-[96px] h-[96px]" src="/images/card-logo-1.svg" alt="card-logo-1" width={0} height={0} />
-                  <h1 className="text-[36px] text-[2B2B39] text-center font-bold">Sejarah & Dasar Hukum Pemilu</h1>
-                  <p className="text-[18px] text-[#5F5F75] text-center m-[32px] font-normal">
-                  Modul ini memberikan gambaran lengkap mengenai sejarah dan evolusi pemilihan umum di Indonesia, serta dasar-dasar hukum yang mengatur proses pemilu. 
-                  </p>
-                </div>
-              </div>
-              <div className="bg-[#F5EFE0] w-[359px] h-[448px] rounded-[16px]">
-                <div className="flex flex-col items-center mt-[37px]">
-                  <Image className="object-cover w-[96px] h-[96px]" src="/images/card-logo-2.svg" alt="card-logo-2" width={0} height={0} />
-                  <h1 className="text-[36px] text-[2B2B39] text-center font-bold">Syarat & Prosedur Bagi Calon</h1>
-                  <p className="text-[18px] text-[#5F5F75] text-center m-[32px] font-normal">
-                  Modul ini menguraikan prosedur dan persyaratan bagi individu yang ingin mencalonkan diri dalam pemilu. 
-                  </p>
-                </div>
-              </div>
-              <div className="bg-[#F5EFE0] w-[359px] h-[448px] rounded-[16px]">
-                <div className="flex flex-col items-center mt-[37px]">
-                  <Image className="object-cover w-[96px] h-[96px]" src="/images/card-logo-3.svg" alt="card-logo-3" width={0} height={0} />
-                  <h1 className="text-[36px] text-[2B2B39] text-center font-bold">Verifikasi & Validasi Data Pemilih</h1>
-                  <p className="text-[18px] text-[#5F5F75] text-center m-[32px] font-normal">
-                  Modul ini fokus pada pentingnya verifikasi dan validasi data pemilih untuk memastikan integritas pemilu. 
-                  </p>
-                </div>
+        <div className="flex flex-auto justify-center">
+          <div className="flex flex-row gap-6">
+            <div className="bg-[#F5EFE0] w-[359px] h-[448px] rounded-[16px]">
+              <div className="flex flex-col items-center mt-[37px] ">
+                <Image className="object-cover w-[96px] h-[96px]" src="/images/card-logo-1.svg" alt="card-logo-1" width={0} height={0} />
+                <h1 className="text-[36px] text-[2B2B39] text-center font-bold">Sejarah & Dasar Hukum Pemilu</h1>
+                <p className="text-[18px] text-[#5F5F75] text-center m-[32px] font-normal">
+                  Modul ini memberikan gambaran lengkap mengenai sejarah dan evolusi pemilihan umum di Indonesia, serta dasar-dasar hukum yang mengatur proses pemilu.
+                </p>
               </div>
             </div>
-           
-                {/* <Swiper
+            <div className="bg-[#F5EFE0] w-[359px] h-[448px] rounded-[16px]">
+              <div className="flex flex-col items-center mt-[37px]">
+                <Image className="object-cover w-[96px] h-[96px]" src="/images/card-logo-2.svg" alt="card-logo-2" width={0} height={0} />
+                <h1 className="text-[36px] text-[2B2B39] text-center font-bold">Syarat & Prosedur Bagi Calon</h1>
+                <p className="text-[18px] text-[#5F5F75] text-center m-[32px] font-normal">
+                  Modul ini menguraikan prosedur dan persyaratan bagi individu yang ingin mencalonkan diri dalam pemilu.
+                </p>
+              </div>
+            </div>
+            <div className="bg-[#F5EFE0] w-[359px] h-[448px] rounded-[16px]">
+              <div className="flex flex-col items-center mt-[37px]">
+                <Image className="object-cover w-[96px] h-[96px]" src="/images/card-logo-3.svg" alt="card-logo-3" width={0} height={0} />
+                <h1 className="text-[36px] text-[2B2B39] text-center font-bold">Verifikasi & Validasi Data Pemilih</h1>
+                <p className="text-[18px] text-[#5F5F75] text-center m-[32px] font-normal">
+                  Modul ini fokus pada pentingnya verifikasi dan validasi data pemilih untuk memastikan integritas pemilu.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* <Swiper
                   slidesPerView={3}
                   spaceBetween={30}
                   pagination={{
@@ -144,15 +168,15 @@ export default function Home() {
                   <SwiperSlide>Slide 8</SwiperSlide>
                   <SwiperSlide>Slide 9</SwiperSlide>
               </Swiper> */}
-       </div>
-       <div className="flex flex-row justify-center mt-[53px]">
-        <div className="bg-[#F5EFE0] w-[195px] h-[48px] rounded-[10px] pt-[11px]">
-                <div className="flex flex-row justify-center items-center">
-                  <p className="text-[18px] text-[#B30D19] mr-[13px]">Modul Lengkap</p> 
-                  <Image className="object-cover w-[12px] h-[24px] " src="/images/angle-right-red.svg" alt="angle-right-red" width={0} height={0} />
-                </div>
         </div>
-       </div>
+        <div className="flex flex-row justify-center mt-[53px]">
+          <div className="bg-[#F5EFE0] w-[195px] h-[48px] rounded-[10px] pt-[11px]">
+            <div className="flex flex-row justify-center items-center">
+              <p className="text-[18px] text-[#B30D19] mr-[13px]">Modul Lengkap</p>
+              <Image className="object-cover w-[12px] h-[24px] " src="/images/angle-right-red.svg" alt="angle-right-red" width={0} height={0} />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="bg-[#FFFCF7] p-[148px]">
         <h1 className="text-[48px] text-center font-bold ">
@@ -160,24 +184,28 @@ export default function Home() {
         </h1>
         <div className="flex flex-row justify-center mt-[79px]">
           <div>
-              <h2 className="text-[30px] text-wrap w-[439px] mb-[15px]">Uji Pengetahuan mu dengan Quiz kami!</h2>
-              <p className="text-wrap w-[409px]">Millions of people come to Campoal to start and sign petitions that boldly call for social change. Become a member today and fuel our mission to empower everyone to create the kind of change they want to see.</p>
-              <br></br>
-              <div className="bg-[#F5EFE0] w-[195px] h-[48px] rounded-[10px] pt-[11px]">
-                <div className="flex flex-row justify-center items-center">
-                  <p className="text-[18px] text-[#B30D19] mr-[13px]">Mulai Quiz</p> 
-                  <Image className="object-cover w-[12px] h-[24px] " src="/images/angle-right-red.svg" alt="angle-right-red" width={0} height={0} />
-                </div>
+            <h2 className="text-[30px] text-wrap w-[439px] mb-[15px]">Uji Pengetahuan mu dengan Quiz kami!</h2>
+            <p className="text-wrap w-[409px]">Millions of people come to Campoal to start and sign petitions that boldly call for social change. Become a member today and fuel our mission to empower everyone to create the kind of change they want to see.</p>
+            <br></br>
+            <div className="bg-[#F5EFE0] w-[195px] h-[48px] rounded-[10px] pt-[11px]">
+              <div className="flex flex-row justify-center items-center">
+                <p className="text-[18px] text-[#B30D19] mr-[13px]">Mulai Quiz</p>
+                <Image className="object-cover w-[12px] h-[24px] " src="/images/angle-right-red.svg" alt="angle-right-red" width={0} height={0} />
               </div>
+            </div>
           </div>
           <div>
-            <Image src="/images/hero-quiz.svg" alt="hero-quiz" width={0} height={0} className="object-scale-down w-[637px]"/>
+            <Image src="/images/hero-quiz.svg" alt="hero-quiz" width={0} height={0} className="object-scale-down w-[637px]" />
           </div>
         </div>
-        
-
       </div>
-        
+
+      <div className="flex flex-col items-center">
+        <h1 className="text-[48px] text-center pt-[75px]">Kenali Kandidatmu!</h1>
+        <div>
+          <CandidateCards candidates={candidates} />
+        </div>
+      </div>
     </div>
   );
 }
