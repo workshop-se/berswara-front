e06332b95ac0dd6c6442d5fb7be06a239c0bfdfc
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-05 08:44:47 +0700

    style(header): add responsibility

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import Image from "next/image";
 import { navItems, authButton } from "@/configs/routes";
 import { logout } from "@/lib/auth";
+import { useState } from "react";
 type HeaderProps = {
   session: any;
 };
 
 const Header: React.FC<HeaderProps> = ({ session }) => {
+  const [toggleDrawer, setToggleDrawer] = useState(false);
 
   const handleClick = () => {
     if (session) {
@@ -18,26 +20,39 @@ const Header: React.FC<HeaderProps> = ({ session }) => {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 flex h-[70px] bg-white shadow-md z-50">
-        <div className="m-auto">
-          <Image className="mx-[12px] h-[51px]" src="/images/logo.svg" width={206} height={36} alt="Logo" />
-        </div>
-        <div className="grow flex justify-center m-auto">
-          {navItems.map((item) => (
-            <Link key={item.name} href={item.url} className="p-[10px]">{item.name}</Link>
-          ))}
-        </div>
-        {session
-          ? <Link onClick={handleClick} href={authButton[3].url} className="m-auto p-[10px] mx-[12px]">{authButton[3].name}</Link>
-          : <div className="m-auto w-[206px] h-[36px] rounded-[10px] bg-red-100 mx-[12px] grid grid-cols-2 overflow-hidden">
-            <Link href={authButton[0].url} className="bg-gray-800 flex items-center justify-center text-white">{authButton[0].name}</Link>
-            <Link href={authButton[1].url} className="bg-red-700 flex items-center justify-center text-white">{authButton[1].name}</Link>
+      <nav className="fixed top-0 left-0 right-0 bg-white shadow-md z-10 flex flex-col lg:flex-row lg:justify-between text-sm font-bold font-['Sen'] py-[27px] lg:py-0 lg:h-[70px] items-center gap-y-[27px]">
+        <Image className="mx-[12px]" src="/images/logo.svg" width={224} height={51} alt="Logo" />
+        <button onClick={() => setToggleDrawer(!toggleDrawer)} className="text-[48px] lg:hidden">=</button>
+        {toggleDrawer && (
+          <div className="flex flex-col items-center justify-center">
+            {navItems.map((item) => (
+              <Link key={item.name} href={item.url} className="p-[10px] my-auto hover:bg-firebrick-0 hover:text-white hover:rounded-[10px]">{item.name}</Link>
+            ))}
           </div>
+        )}
+        <NavBar className="hidden lg:flex flex-row gap-x-[20px]" />
+        {!session || session?.error
+          ? <div className="my-auto w-[206px] h-[36px] rounded-[10px] mx-[12px] flex overflow-hidden text-center">
+            <Link href={authButton[0].url} className="grow bg-darkslategray text-white hover:scale-110 flex flex-col justify-center">{authButton[0].name}</Link>
+            <Link href={authButton[1].url} className="grow bg-firebrick-0 text-white hover:scale-110 flex flex-col justify-center">{authButton[1].name}</Link>
+          </div>
+          : <Link onClick={handleClick} href={authButton[3].url} className="m-auto p-[10px] mx-[12px] hover:text-white hover:bg-firebrick-0 rounded-[10px]">{authButton[3].name}</Link>
         }
       </nav>
-      <nav className="h-[70px]"></nav>
+      <nav className="h-[210px] lg:h-[70px]"></nav>
     </>
   );
 }
 
+function NavBar({className}: {className: string}) {
+  return (
+    <div className={className}>
+      {navItems.map((item) => (
+        <Link key={item.name} href={item.url} className="p-[10px] my-auto hover:bg-firebrick-0 hover:text-white hover:rounded-[10px]">{item.name}</Link>
+      ))}
+    </div>
+  )
+}
+
+// <nav className="h-[70px]"></nav>
 export default Header;
