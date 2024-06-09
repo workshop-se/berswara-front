c43eae7b0b767f0ea6e8c4c95ea0648e4a9dc1f7
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-09 19:11:16 +0700

    feat(forum): add active navigation

--- src/app/(navigation)/forum/template.tsx
+++ src/app/(navigation)/forum/template.tsx
@@ -1,17 +1,54 @@
+'use client'
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const search = { title: "Search", icon: "/icons/search.svg", url: "/forum/search" }
+  const search = {
+    title: "Search",
+    icon: "/icons/search.svg",
+    icon_red: "/icons/search-red.svg",
+    url: "/forum/search"
+  }
   const menus = [
-    { title: "Questions", icon: "/icons/list.svg", url: "/" },
-    { title: "Tag", icon: "/icons/tag.svg", url: "/forum/tags" },
-    { title: "Ranking", icon: "/icons/award.svg", url: "/news/ranking" },
+    {
+      title: "Questions",
+      icon: "/icons/list.svg",
+      icon_red: "/icons/list-red.svg",
+      url: "/"
+    },
+    {
+      title: "Tag",
+      icon: "/icons/tag.svg",
+      icon_red: "/icons/tag-red.svg",
+      url: "/forum/tags"
+    },
+    {
+      title: "Ranking",
+      icon: "/icons/award.svg",
+      icon_red: "/icons/award-red.svg",
+      url: "/news/ranking"
+    },
   ]
   const navs = [
-    { title: "Your Questions", icon: "/icons/help-circle.svg", url: "/forum/yourquestions" },
-    { title: "Your Answers", icon: "/icons/message-circle.svg", url: "/forum/answers" },
-    { title: "Your likes & votes", icon: "/icons/heart.svg", url: "/forum/likesandvotes" },
+    {
+      title: "Your Questions",
+      icon: "/icons/help-circle.svg",
+      icon_red: "/icons/help-circle-red.svg",
+      url: "/forum/yourquestions"
+    },
+    {
+      title: "Your Answers",
+      icon: "/icons/message-circle.svg",
+      icon_red: "/icons/message-circle-red.svg",
+      url: "/forum/answers"
+    },
+    {
+      title: "Your likes & votes",
+      icon: "/icons/heart.svg",
+      icon_red: "/icons/heart-red.svg",
+      url: "/forum/likesandvotes"
+    },
   ]
 
   return (
@@ -28,16 +65,24 @@ export default function Template({ children }: { children: React.ReactNode }) {
   )
 }
 
-function Item({ item }: { item: { title: string, icon: string, url: string } }) {
+function Item({ item }: { item: { title: string, icon: string, icon_red: string, url: string } }) {
+  const pathname = usePathname();
+  const isActive = pathname.includes(item.url);
+
   return (
-    <Link href={item.url} className="px-[50px] hover:bg-firebrick-0/[0.13] h-[41px] flex gap-x-[12px]">
-      <Image src={item.icon} width={18} height={18} alt={item.title}></Image>
+    <Link href={item.url} className={`h-[41px] flex gap-x-[12px] ${isActive ? 'bg-firebrick-0/[0.13] text-firebrick-0' : 'pl-[50px] hover:bg-firebrick-0/[0.13] text-black'}`}>
+      {isActive && <div className="w-[5px] mr-[33px] h-full bg-firebrick-0"></div>}
+      {
+        isActive
+          ? <Image src={item.icon_red} width={18} height={18} alt={item.title}></Image> 
+          : <Image src={item.icon} width={18} height={18} alt={item.title}></Image>
+      }
       <h1 className="text-[13px] my-auto">{item.title}</h1>
     </Link>
   )
 }
 
-function Menu({ title, items }: { title: string, items: { title: string, icon: string, url: string }[] }) {
+function Menu({ title, items }: { title: string, items: { title: string, icon: string, icon_red: string, url: string }[] }) {
   return (
     <div className="flex flex-col gap-y-[10px]">
       <h1 className="text-[13px] text-gray px-[50px]">{title}</h1>
