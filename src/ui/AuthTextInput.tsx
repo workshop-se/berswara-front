23570d0d297f98581606c1ab4aae2d88bc9de960
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-10 12:20:39 +0700

    fix(auth): fix AuthTextInput reference error

--- src/ui/AuthTextInput.tsx
+++ src/ui/AuthTextInput.tsx
@@ -1,8 +1,25 @@
-export default function AuthTextInput({ label, type, id, name, ref, onInput }: { label: string, type: string, id: string, name: string, ref: React.RefObject<HTMLInputElement>, onInput: () => void }) {
-  return (
-    <div className="flex flex-col gap-[2.92px]">
-      <label htmlFor={id} className="text-dimgray text-[12px]">{label}</label>
-      <input ref={ref} onInput={onInput} type={type} id={id} name={name} className="h-[41px] ring-1 ring-silver rounded-[9px] px-2" />
-    </div>
-  );
+import { forwardRef } from 'react';
+
+interface AuthTextInputProps {
+  label: string;
+  type: string;
+  id: string;
+  name: string;
+  onInput: () => void;
 }
+
+const AuthTextInput = forwardRef<HTMLInputElement, AuthTextInputProps>(
+  ({ label, type, id, name, onInput }, ref) => {
+    return (
+      <div className="flex flex-col gap-[2.92px]">
+        <label htmlFor={id} className="text-dimgray text-[12px]">{label}</label>
+        <input ref={ref} onInput={onInput} type={type} id={id} name={name} className="h-[41px] ring-1 ring-silver rounded-[9px] px-2" />
+      </div>
+    );
+  }
+);
+
+AuthTextInput.displayName = 'AuthTextInput';
+
+export default AuthTextInput;
+
